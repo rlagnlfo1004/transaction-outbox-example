346675cb9a752e17dc9411cb5fb8ae1c7f855cea
휘래 김 <hramst0618@gmail.com>
2026-01-03 10:00:00 +0900

    init

---
+++ src/main/java/com/example/transactionoutbox/TransactionOutboxApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionoutbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionOutboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionOutboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/transactionoutbox/TransactionOutboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionoutbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionOutboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
